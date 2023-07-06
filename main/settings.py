7a5375c08fb34ec7db7ecb045d31b1bbc9387378
SodiqNaimov <sodiq.naimov@mail.ru>
2023-07-07 02:18:56 +0500

    b

--- main/settings.py
+++ main/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-w=oo8-hw^3jvo=b*1l)3qjcm6+bcg&hj@9+zgrv_rxpk6mwg+p"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
